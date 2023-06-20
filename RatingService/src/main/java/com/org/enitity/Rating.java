416678f5216cca72d52c017a9e091d08ea764472
shaikh shahzade <shahzade10@gmail.com>
2023-06-20 23:58:59 +0530

    Rating service completed

--- RatingService/src/main/java/com/org/enitity/Rating.java
+++ RatingService/src/main/java/com/org/enitity/Rating.java
@@ -1,12 +1,18 @@
 package com.org.enitity;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Service;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Document("Rating")
 public class Rating {
     @Id
     private String ratingId;
     private Integer rating;
-    private String Feedback;
+    private String feedback;
 }
--- RatingService/src/main/java/com/org/serviceimpl/RatingServiceImpl.java
+++ RatingService/src/main/java/com/org/serviceimpl/RatingServiceImpl.java
@@ -6,9 +6,10 @@ import com.org.service.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class RatingServiceImpl implements RatingService {
     @Autowired
     private RatingRepository ratingRepository;
